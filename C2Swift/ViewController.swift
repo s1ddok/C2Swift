96282c8193bc58c5ded97fc90cf4ce8eac9828a4
s1ddok <siddok@gmail.com>
2017-04-27 10:16:26 +0200

    Initial Commit

---
+++ C2Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  C2Swift
+//
+//  Created by Andrey Volodin on 27.04.17.
+//  Copyright © 2017 s1ddok. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
